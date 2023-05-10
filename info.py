5ccaa8bd2e4ae7cd3c78174888c8a7badfadb110
testheroku24k <133033304+testheroku24k@users.noreply.github.com>
2023-05-10 18:20:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+API_ID       = 25632035
+API_HASH     = "896d8f9929d3e00d2dae14646329fe3b"
+BOT_TOKEN    = "6123477306:AAFz8wf7oWwGhrV1AMx2f2fJheQESbpnqbs"
+SESSION      = "BQGHHSMAgj8z421bBgARghninMVC-iFAiEwlvwkr_eJDo4Ms5kFRsmiYPvEzXf-K4j_u0-EAKWeICzAy67ejZIQvIzaf7Pbp1Hi8mv4TRHnMexu_lN49UW5eJUncHhEeFrZzMHYEJZdq3n1aWIaPUvhfzWW6CTdM12ew_gVcLJqV5Y_5oRk6WSsf6tCEFfO8wmae_EWBR9EDJv4lwQ136lLJhD-awAmY-LlmDLfxrrqhcyrDwWesubY9DSOWA4hUoU2hMfldmG6iw1p0Fgzry25erpKDzwLJaG8r-WrkyqSzY6J8RHEwqGM49_vVu-_pf9araoSLrH31xvQZCHRpQHDjA3X2MQAAAAFrQPVfAA"
+DATABASE_URI = "mongodb+srv://clientstamil4k:Ruban9124@postfinder.2fd5egv.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001936841720
+ADMIN        = 6094386527
+CHANNEL      = "@Snowsbotz"
